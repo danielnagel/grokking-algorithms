2775da583ffa128362a49604b2ba80cbc02d6ba2
Daniel Nagel <danielnagel@mailbox.org>
2025-02-03 19:44:49 +0100

    updated sum function

--- ch4/sum.ts
+++ ch4/sum.ts
@@ -1,1 +1,1 @@
-export const sum = (arr: number[]): number => !arr.length ? 0 : arr.slice(0, 1)[0] + sum(arr.slice(1));
+export const sum = (arr: number[]): number => !arr.length ? 0 : arr[0] + sum(arr.slice(1));
